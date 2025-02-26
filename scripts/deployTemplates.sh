8b5bb280f3f2ff1949b535e4e59ddfe070c59f2b
Mike Brandl <mike.brandl@cityfibre.com>
2025-02-26 11:51:00 +0000

    Remove Duplication

--- scripts/deployTemplates.sh
+++ scripts/deployTemplates.sh
@@ -17,30 +17,17 @@ source $SCRIPT_DIR/functions.sh
 checkRequiredEnvVar CICD_ACCESS_TOKEN               "$CICD_ACCESS_TOKEN"
 checkRequiredEnvVar CICD_REPOSITORY_PATH            "$CICD_REPOSITORY_PATH"
 
-if [ "$CICD_PLATFORM" = "" ]; then
-  CICD_PLATFORM=gitlab
-  if [ -z "$CICD_SERVER_HOST" ]; then
-    CICD_SERVER_HOST="gitlab.com"
-  fi
-elif [ "$CICD_PLATFORM" = "github" ]; then
-  if [ -z "$CICD_SERVER_HOST" ]; then
-    CICD_SERVER_HOST="github.com"
-  fi
-elif [ "$CICD_PLATFORM" = "bitbucket" ]; then
-  if [ -z "$CICD_SERVER_HOST" ]; then
-    CICD_SERVER_HOST="bitbucket.org"
-  fi
-fi
-echo "The CI/CD platform is: $CICD_PLATFORM"
-
 git config --global user.name "$GIT_USERNAME"
 git config --global user.email $GIT_USER_EMAIL
 
-git fetch
-
-git checkout main
+if [ "$CICD_BRANCH" = "" ]; then
+  CICD_BRANCH=main
+fi
+echo "Checkout branch: $CICD_BRANCH"
+git checkout -B $CICD_BRANCH
 
 python $SCRIPT_DIR/deployConnectorTemplates.py
+echo "Script Complete, Committing Config."
 
 git add config.*
 
