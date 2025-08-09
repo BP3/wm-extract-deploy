eb6cd49621eedf1e7785e936d439323c942777dd
Joe Warner <awarner@bp-3.com>
2025-08-09 11:29:28 +0100

    Debug pulling images

--- integration-tests/load-images.sh
+++ integration-tests/load-images.sh
@@ -12,16 +12,19 @@
 #
 ############################################################################
 
-composeFile=$1
 envFile=.env
 
 # Load environment variables
+echo "Loading environment variables"
 . ./$envFile
 
+env | sort
+
 # Read compose file to find which images we need
-grep 'image:' $composeFile | while read imageLine; do
+cat *-compose.yaml | grep -v '^#' | grep 'image:' | sort -u | while read imageLine; do
   imgRef=`echo $imageLine | cut -d' ' -f2`
   # Use eval to de-reference the version EnvVar
   image=`eval echo "$imgRef"`
+  echo "Pulling: $image"
   docker pull $image
 done
