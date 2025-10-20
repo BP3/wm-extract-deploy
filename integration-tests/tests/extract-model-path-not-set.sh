800e4fd4c079de705c54054036e1a78a9b6c720e
Justin Phillips <jphillips@bp-3.com>
2025-10-20 11:31:35 +0100

    Updated git email

--- integration-tests/tests/extract-model-path-not-set.sh
+++ integration-tests/tests/extract-model-path-not-set.sh
@@ -85,7 +85,7 @@ When () {
     -e CICD_REPOSITORY_PATH="$GITHUB_WORKSPACE" \
     -e CICD_SERVER_HOST="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" \
     -e GIT_USERNAME="$GITHUB_ACTOR" \
-    -e GIT_USER_EMAIL="$GITHUB_ACTOR@bp-3.com" \
+    -e GIT_USER_EMAIL="$GITHUB_ACTOR+$GITHUB_ACTOR_ID@users.noreply.github.com" \
       --entrypoint /bin/sh $IMAGE_NAME:$IMAGE_REF
 
   echo Sleep for a few seconds whilst docker container comes up ...
