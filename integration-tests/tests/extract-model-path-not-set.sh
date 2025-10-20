a92aec86ef2389c3663f5ca5d4cefd628de4f282
Justin Phillips <jphillips@bp-3.com>
2025-10-20 09:53:42 +0100

    Added branch param so we don't default to main

--- integration-tests/tests/extract-model-path-not-set.sh
+++ integration-tests/tests/extract-model-path-not-set.sh
@@ -106,6 +106,7 @@ When () {
     -e OAUTH2_TOKEN_URL=http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token \
     -e CAMUNDA_WM_PROJECT="$project_id" \
     -e CAMUNDA_WM_HOST="localhost:8070" \
+    -e CICD_BRANCH="$GITHUB_REF" \
     -e CICD_ACCESS_TOKEN=dummy \
     -e CICD_REPOSITORY_PATH="$GITHUB_WORKSPACE" \
     -e CICD_SERVER_HOST="$GITHUB_SERVER_URL" \
