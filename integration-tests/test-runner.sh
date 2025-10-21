8d413c4bbed4fa7f99bafaea45afdd7a6523107f
Justin Phillips <jphillips@bp-3.com>
2025-10-21 14:41:57 +0100

    Removed debug output

--- integration-tests/test-runner.sh
+++ integration-tests/test-runner.sh
@@ -71,18 +71,11 @@ run_test () {
 }
 
 if [ $composeFile = "extract-compose.yaml" ]; then
-  # TODO Remove debug once working
-  echo "*** Found tests:\n$('ls' -1S $TESTSDIR/tests/extract*.sh)"
   'ls' -1S $TESTSDIR/tests/extract*.sh | while read tst; do
     next_tst=`basename $tst`
-    echo "*** Running test $next_tst"
     run_test $composeFile $next_tst
-
-    # TODO Remove debug once working
-    echo "*** Test $next_tst has completed"
   done
 
-  echo "*** Done!"
 fi
 
 if [ $composeFile = "deploy-compose.yaml" ]; then
