c7fee5847d796114bc8161c6b0cf2afdd953b552
Justin Phillips <jphillips@bp-3.com>
2025-10-21 11:31:16 +0100

    Added back container stop and remove

--- integration-tests/tests/extract-1.sh
+++ integration-tests/tests/extract-1.sh
@@ -123,8 +123,8 @@ When () {
   docker exec $DOCKER_TTY_OPTS -w /local wmed /app/scripts/extractDeploy.sh extract
   echo "*** Exit code = $?"
 #  docker container cp wmed:/local $TESTSDIR/$TESTNAME
-#  docker container stop wmed
-#  docker container rm wmed
+  docker container stop wmed
+  docker container rm wmed
 #
 #  # Move the data where we want it
 #  mv $TESTSDIR/$TESTNAME/local/* $TESTSDIR/$TESTNAME
