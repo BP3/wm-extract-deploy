d53e7edca0da0a9bd6c9a087db5a96c067e2e5a9
Justin Phillips <jphillips@bp-3.com>
2025-10-21 11:21:05 +0100

    Removed docker exec

--- integration-tests/tests/extract-1.sh
+++ integration-tests/tests/extract-1.sh
@@ -120,8 +120,8 @@ When () {
   echo Sleep for a few seconds whilst docker container comes up ...
   sleep 5
 
-  docker exec $DOCKER_TTY_OPTS -w /local wmed /app/scripts/extractDeploy.sh extract
-  echo "*** Exit code = $?"
+#  docker exec $DOCKER_TTY_OPTS -w /local wmed /app/scripts/extractDeploy.sh extract
+#  echo "*** Exit code = $?"
 #  docker container cp wmed:/local $TESTSDIR/$TESTNAME
 #  docker container stop wmed
 #  docker container rm wmed
