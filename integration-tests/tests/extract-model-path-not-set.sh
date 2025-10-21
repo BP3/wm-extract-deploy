e334ebe3d353a86b15fe6012b4f58fb300cd88f2
Justin Phillips <jphillips@bp-3.com>
2025-10-21 13:51:58 +0100

    Updated var back to MODEL_PATH and added echo

--- integration-tests/tests/extract-model-path-not-set.sh
+++ integration-tests/tests/extract-model-path-not-set.sh
@@ -92,7 +92,7 @@ When () {
 Then () {
   echo "$TESTNAME: Then"
 
-  model_path=$(docker exec wmed printenv CAMUNDA_WM_HOST)
+  model_path=$(echo docker exec wmed printenv MODEL_PATH)
   echo "The MODEL_PATH has been set to $model_path"
 
   assert_equals "$model_path" "."
