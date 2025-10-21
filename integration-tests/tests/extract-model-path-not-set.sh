4d704645fa0d981384395f7d2cd54387a6eccd5d
Justin Phillips <jphillips@bp-3.com>
2025-10-21 14:20:21 +0100

    Fixed TTY OPTS var name

--- integration-tests/tests/extract-model-path-not-set.sh
+++ integration-tests/tests/extract-model-path-not-set.sh
@@ -92,7 +92,7 @@ When () {
 Then () {
   echo "$TESTNAME: Then"
 
-  model_path=$(docker exec "$DOCKER_TTY_OPT" wmed printenv MODEL_PATH)
+  model_path=$(docker exec "$DOCKER_TTY_OPTS" wmed printenv MODEL_PATH)
   echo "The MODEL_PATH has been set to $model_path"
 
   assert_equals "$model_path" "."
