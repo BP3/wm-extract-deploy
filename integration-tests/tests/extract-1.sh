3d8183c43e5bf18fcb63c46051db1832dafafdf9
Justin Phillips <jphillips@bp-3.com>
2025-10-21 11:44:33 +0100

    Added back docker exec

--- integration-tests/tests/extract-1.sh
+++ integration-tests/tests/extract-1.sh
@@ -120,9 +120,9 @@ When () {
   echo Sleep for a few seconds whilst docker container comes up ...
   sleep 5
 
-#  docker exec $DOCKER_TTY_OPTS -w /local wmed /app/scripts/extractDeploy.sh extract
-#  echo "*** Exit code = $?"
-#  docker container cp wmed:/local $TESTSDIR/$TESTNAME
+  docker exec $DOCKER_TTY_OPTS -w /local wmed /app/scripts/extractDeploy.sh extract
+  echo "*** Exit code = $?"
+  docker container cp wmed:/local $TESTSDIR/$TESTNAME
   docker container stop wmed
   docker container rm wmed
 #
